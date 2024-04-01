8c04fae091b031dee1d6becc5ff00857cb9464a1
Gauravkandel <gauravkandell990@gmail.com>
2024-04-01 17:02:28 +0545

    callbacks

--- config/services.php
+++ config/services.php
@@ -16,7 +16,7 @@ return [
     'google' => [
         'client_id' => env("GOOGLE_CLIENT_ID"),
         'client_secret' => env("GOOGLE_CLIENT_SECRET"),
-        'redirect' => "http://gbmobiles-production.up.railway.app/callback"
+        'redirect' => env('APP_URL') . "/callback"
     ],
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
